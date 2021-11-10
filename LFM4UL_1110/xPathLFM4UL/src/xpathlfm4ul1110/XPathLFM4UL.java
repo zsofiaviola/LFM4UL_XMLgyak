efefc183a487998587e5f17a6e3f218eaa9be390
zsofiaviola <zsofiaviola@gmail.com>
2021-11-10 11:20:40 +0100

    11.10. gyakorlat

---
+++ LFM4UL_1110/xPathLFM4UL/src/xpathlfm4ul1110/XPathLFM4UL.java
@@ -0,0 +1,72 @@
+package xpathlfm4ul1110;
+
+import java.io.File;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class XPathLFM4UL {
+	
+	public static void main(String[]args) {
+		
+		try {
+			File xmlFile = new File("studentLFM4UL.xml");
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = factory.newDocumentBuilder();
+			
+			Document document = dBuilder.parse(xmlFile);
+			document.getDocumentElement().normalize();
+			
+			XPath xPath = XPathFactory.newInstance().newXPath();
+			String expression = "class/student";
+			
+			NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(document, XPathConstants.NODESET);
+			
+			for(int i=0; i<nodeList.getLength(); i++) {
+				Node node = nodeList.item(i);
+				
+				System.out.println("Aktualis elem:" + node.getNodeName());
+				
+				if(node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("student")) {
+					
+					Element element = (Element) node;
+					
+					System.out.println("Hallgató ID: " + element.getAttribute("id"));
+					System.out.println("Keresztnév: " + element.getElementsByTagName("keresztnev").item(0).getTextContent());
+					System.out.println("Vezeteknev: " + element.getElementsByTagName("vezeteknev").item(0).getTextContent());
+					System.out.println("Becenev: " + element.getElementsByTagName("becenev").item(0).getTextContent());
+					System.out.println("Kor: " + element.getElementsByTagName("kor").item(0).getTextContent());
+				}
+			}
+		
+	} catch(ParserConfigurationException e) {
+        e.printStackTrace();
+} catch(SAXException e) {
+    e.printStackTrace();
+}
+    catch(IOException e) {
+        e.printStackTrace();
+        
+    } catch(XPathExpressionException e) {
+            e.printStackTrace();
+    } 
+} 
+
+}
